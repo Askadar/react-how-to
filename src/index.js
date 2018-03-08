b8355a3575611cd307c0665011e5c12b4db997b8
Антон Обрядин <askadar.ru@gmail.com>
2018-03-09 00:12:21 +0300

    Functional "refactor"

--- src/index.js
+++ src/index.js
@@ -1,14 +1,10 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
-class ColoredDiv extends React.Component{
-	render() {
-		const {color, fontSize: fz, rows } = this.props;
-		return (<div style={{background: this.props.bgColor, color, fontSize: fz}}>
-			{rows.map(row => <li>{row}</li>)}
-		</div>);
-	}
-}
+const ColoredDiv = ({color, fontSize: fz, rows, ...props }) => 
+<div style={{background: props.bgColor, color, fontSize: fz}}>
+	{rows.map(row => <li>{row}</li>)}
+</div>
 
 ReactDOM.render(<ColoredDiv color="#ab00ff" bgColor="white" fontSize="14px" rows={['Me colored, yay!', 'Me colored, nope second try!', 'No random is it!', ]}/>, document.getElementById('root'));
 
