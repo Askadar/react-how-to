d24a6b2adcc0b2c40040816de81d7c83721108d1
Антон Обрядин <askadar.ru@gmail.com>
2018-03-08 19:30:49 +0300

    Stateless component with parametrized 'innerHTML'

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
 class ColoredDiv extends React.Component{
-    render() {
-        const {color, fontSize: fz } = this.props;
-        return <div style={{background: this.props.bgColor, color, fontSize: fz}}>Me colored, yay!</div>
-    }
+	render() {
+		const {color, fontSize: fz } = this.props;
+		return (<div style={{background: this.props.bgColor, color, fontSize: fz}}>
+			{Math.random() >= 0.5 ? 'Me colored, yay!' : 'Me no random!'}
+		</div>);
+	}
 }
 
 ReactDOM.render(<ColoredDiv color="red" bgColor="green" fontSize="14px"/>, document.getElementById('root'));
